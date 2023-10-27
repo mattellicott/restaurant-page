2b3a3d0bdd0bf6fec4c9a754a863588af60b8443
Matt Ellicott <mattellicott@gmail.com>
2023-10-27 18:14:05 -0500

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Restaurant Page",
+    }),
+  ],
+};
