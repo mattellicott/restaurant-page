c2578583883e55e2525f5c7140b037b726be44c0
Matt Ellicott <mattellicott@gmail.com>
2023-10-28 13:58:06 -0500

    Rename entry point

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,9 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: "./src/index.js",
+    index: "./src/index.js",
   },
   output: {
     filename: "[name].js",
