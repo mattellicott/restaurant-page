451b53f2bb32e4860cbe7b685ed16ca7cde8f169
Matt Ellicott <mattellicott@gmail.com>
2023-10-29 20:18:28 -0500

    Fix menu event handlers

---
+++ src/modules/content.js
@@ -0,0 +1,89 @@
+import { makeElement } from "./functions";
+import { home } from "./home";
+import { menu } from "./menu";
+import { contact } from "./contact";
+import siteData from "./siteData.json";
+
+class Content {
+  constructor(data) {
+    this.data = data;
+    this.mainPages = { home, menu, contact };
+    this.currentMainPage = this.mainPages.home.page;
+    this.menuTabElements = [];
+
+    this.header = this.createHeader();
+    this.main = this.createMain();
+    this.footer = this.createFooter();
+    this.content = this.createContent();
+  }
+
+  createContent() {
+    const wrapperElement = makeElement("div", "id", "content");
+
+    wrapperElement.appendChild(this.header);
+    wrapperElement.appendChild(this.main);
+    wrapperElement.appendChild(this.footer);
+
+    return wrapperElement;
+  }
+
+  createHeader() {
+    const wrapperElement = makeElement("header");
+
+    wrapperElement.appendChild(this.createHeaderLogo());
+    wrapperElement.appendChild(this.createHeaderTabs());
+
+    return wrapperElement;
+  }
+
+  createHeaderLogo() {
+    const logoElement = makeElement("div", "id", "logo", this.data.logoText);
+
+    return logoElement;
+  }
+
+  createHeaderTabs() {
+    const wrapperElement = makeElement("div", "id", "menuTabs");
+
+    for (const tab of this.data.menuTabs) {
+      const button = makeElement("button", "class", "menuTab", tab);
+      if (tab === "Home") button.classList.add("active");
+
+      button.addEventListener("click", (e) => {
+        for (const tab of this.menuTabElements) {
+          if (tab === e.target && !tab.classList.contains("active"))
+            tab.classList.add("active");
+          else if (tab !== e.target) tab.classList.remove("active");
+        }
+
+        const newPage = this.mainPages[tab.toLowerCase()].page;
+        this.main.replaceChild(newPage, this.currentMainPage);
+        this.currentMainPage = newPage;
+      });
+
+      this.menuTabElements.push(button);
+      wrapperElement.appendChild(button);
+    }
+
+    return wrapperElement;
+  }
+
+  createMain() {
+    const wrapperElement = makeElement("main");
+
+    wrapperElement.appendChild(this.currentMainPage);
+
+    return wrapperElement;
+  }
+
+  createFooter() {
+    const wrapperElement = document.createElement("footer");
+
+    wrapperElement.innerHTML =
+      "Copyright © " + new Date().getFullYear() + " " + this.data.copyrighter;
+
+    return wrapperElement;
+  }
+}
+
+export const website = new Content(siteData);
