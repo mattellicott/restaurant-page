ff7bfe3550a1d1557367c8b428f2551fe2da9acb
Matt Ellicott <mattellicott@gmail.com>
2023-10-29 20:13:18 -0500

    .

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -9,10 +10,23 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    assetModuleFilename: (pathData) => {
+      const filepath = path
+        .dirname(pathData.filename)
+        .split("/")
+        .slice(1)
+        .join("/");
+      return `${filepath}/[name].[hash][ext][query]`;
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Restaurant Page",
+      // favicon: "./src/assets/favicon.ico",
+    }),
+    new FaviconsWebpackPlugin({
+      logo: "./src/assets/favicon.png",
+      mode: "light",
     }),
   ],
   module: {
@@ -20,6 +34,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
